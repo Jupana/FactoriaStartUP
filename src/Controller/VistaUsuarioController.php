0d2f46d9af4903da345a97c6baf78c37add2982e
Nicol <nicoletagologan@yahoo.com>
2019-01-22 18:59:04 +0100

    Nueva Vista_Datos_Personales

--- src/Controller/VistaUsuarioController.php
+++ src/Controller/VistaUsuarioController.php
@@ -12,9 +12,34 @@ class VistaUsuarioController extends Controller
     /**
      * @Route("/", name="index")
      */
-    public function index(Request $request): Response
+    public function index_vista(Request $request): Response
     {
         $em = $this->getDoctrine()->getManager();
-        return $this->render('vista_usuario/index.html.twig');
+        return $this->render('vista_usuario/index_vista.html.twig');
+    }
+    public function datos_personales(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        return $this->render('vista_usuario/datos_Personales.html.twig');
+    }
+    public function datos_profesionales(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        return $this->render('vista_usuario/datos_Profesionales.html.twig');
+    }
+    public function datos_proyectos(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        return $this->render('vista_usuario/datos_Proyectos.html.twig');
+    }
+    public function datos_propuestas(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        return $this->render('vista_usuario/datos_Propuestas.html.twig');
+    }
+    public function datos_cuenta(Request $request): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        return $this->render('vista_usuario/datos_Cuenta.html.twig');
     }
 }
