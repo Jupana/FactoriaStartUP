8c0d322bdeccaedf3d1e8b1f27bf342276c14ce6
liviu.todoran <liviu.todoran@webfg.com>
2019-05-14 12:30:38 +0200

    New Query for Distance in comments UserRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -52,6 +52,22 @@ class UserRepository extends ServiceEntityRepository
     */
 
     //good query SELECT ( 6371 * acos( cos( radians(40.5089) ) * cos( radians( user_latitud ) ) * cos( radians( user_longitud ) - radians(-3.66544) ) + sin( radians(40.5089) ) * sin(radians(user_latitud)) ) ) AS distance  FROM     fsu_users  HAVING     distance < 25  ORDER BY     distance;
+   
+   /*Another QUERY
+   SELECT user_name, user_latitud, user_longitud, 6371 * 2 * 
+          ASIN(SQRT( POWER(SIN((40.5089 - user_latitud)*pi()/180/2),2)
+          +COS(40.5089*pi()/180 )*COS(user_latitud*pi()/180)
+          *POWER(SIN((-3.66544-user_longitud)*pi()/180/2),2))) 
+          as distance FROM fsu_users WHERE 
+          user_longitud between (-3.66544-15/cos(radians(40.5089))*69) 
+          and (-3.66544+15/cos(radians(40.5089))*69) 
+          and user_latitud between (40.5089-(15/69)) 
+          and (40.5089+(15/69)) 
+          having distance < 15 ORDER BY distance limit 100
+   
+   
+   */
+   
     public function findByDistance($lat,$long, $distance){
         return $this->createQueryBuilder('p')
             ->addSelect(
