6aae16f5b20602442c087d2f8f99f8620e971eb7
Liviu Todoran <liviutodoran@Livius-MacBook-Pro.local>
2019-09-02 21:24:33 +0200

    Add Needs and Contribute to Admin Controller

--- src/Controller/AdminProjectsController.php
+++ src/Controller/AdminProjectsController.php
@@ -6,10 +6,12 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\NeedsProjectRepository;
+use App\Repository\ContributeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+
 
 
 class AdminProjectsController extends AbstractController
@@ -34,25 +36,38 @@ class AdminProjectsController extends AbstractController
     */
     private $entityManager;
 
+    /**
+    * @var NeedRepository
+    */
+    private $needsRepository;
+
+    /**
+    * @var ContributeRepository
+    */
+    private $contributeRepository;
+
     public function __construct(
         \Twig_Environment $twig, 
         ProjectRepository $projectRepository,
         FormFactoryInterface $formFactory,
         EntityManagerInterface $entityManager,        
-        UserRepository $userRepository
+        UserRepository $userRepository,
+        NeedsProjectRepository $needsRepository,
+        ContributeRepository $contributeRepository
         ) {
         $this->twig = $twig;
         $this->projectRepository = $projectRepository;
         $this->userRepository =$userRepository;
         $this->formFactory = $formFactory;
-        $this->entityManager = $entityManager;        
+        $this->entityManager = $entityManager;  
+        $this->needsRepository = $needsRepository;  
+        $this->contributeRepository = $contributeRepository;    
     }
   
     public function index()
     {
         $projects = $this->projectRepository->findAll();
-        dump($this->getUser()->getRoles());
-    
+      
         return new Response(
             $this->twig->render(
                 'admin/index.html.twig',
@@ -67,11 +82,15 @@ class AdminProjectsController extends AbstractController
     { 
         
         $project = $this->projectRepository ->find($id);
+        $needsProject = $this->needsRepository->findBy(['needs_project'=>$id]);
+        $contributeProjects = $this->contributeRepository->findBy(['contribute_project'=>$id]);
         return new Response(
             $this->twig->render(
                 'admin/project.html.twig',
                 [
-                    'project' => $project                        
+                    'project' => $project ,
+                    'needsProject'  =>$needsProject ,
+                    'contributeProjects' =>$contributeProjects                
                 ]
             )
         ); 
