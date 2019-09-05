afb931b7eb6f4237155a2afc719b853b9f605856
Nicol <nicoletagologan@yahoo.com>
2019-09-05 17:36:14 +0200

    messagesForm

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Form\MessageType;
+use App\Entity\Message;
 use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class MessageController extends AbstractController
 {
@@ -30,13 +32,38 @@ class MessageController extends AbstractController
     }
 
   
-    public function message($id)
+    public function message($id,Request $request)
     {   
         $userMessages = $this->messageRepository->findMessage($this->getUser()->getId());
         $message = $this->messageRepository->findBy(['conversation_id' =>$id]);
+
+        $newMessage = new Message();
+        $form = $this->createForm(
+            MessageType::class,
+            $newMessage
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {        
+            $conversation_id = $newMessage->getConversationId();
+            $type = $newMessage->getType();
+            $text = $newMessage->getText();
+            $time = $newMessage->setTime(new \DateTime());
+            $user_sender = $newMessage->getUserSender();
+            $user_recipient = $newMessage->getUserRecipient();
+            $interest_project = $newMessage->getInterestProject();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newMessage);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('list-messages');
+        }
+
         return $this->render('messages/message.html.twig', [
             'userMessages' => $userMessages,
             'message' => $message,
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -3,24 +3,53 @@
 namespace App\Form;
 
 use App\Entity\Message;
+use App\Entity\Profil;
+use App\Entity\Project;
+use App\Entity\User;
+use App\Entity\InterestProfile;
+use App\Entity\InterestProject;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('conversation_id')
-            ->add('type')
-            ->add('text')
-            ->add('time')
-            ->add('user_sender')
-            ->add('user_recipient')
-            ->add('interest_profil')
-            ->add('interest_project')
-        ;
+            ->add('conversation_id',TextType::class)
+            ->add('type',TextType::class)
+            ->add('text',TextareaType::class,['label' => 'Envia un mensaje...'])
+            ->add('time', DateType::class)
+            ->add('user_sender',EntityType::class,[
+                'class' => User::class,                      
+                'choice_label' => function($user){
+                        return $user->getId(); 
+                    }])
+            ->add('user_recipient',EntityType::class,[
+                'class' => User::class,                      
+                'choice_label' => function($user){
+                        return $user->getId(); 
+                    }])
+            ->add('interest_profil',EntityType::class,[
+                'class' => InterestProfile::class,                      
+                'choice_label' => function($InterestProfile){
+                        return $InterestProfile->getId(); 
+                    },
+                'required'=>false ])
+            ->add('interest_project',EntityType::class,[
+                'class' => InterestProject::class,                      
+                'choice_label' => function($InterestProject){
+                        return $InterestProject->getId(); 
+                    },
+                'required'=>false ])
+            ->add('enviar', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
