08260a2699314be049df0920d07d5d8b881735bb
Nicol <nicoletagologan@yahoo.com>
2019-09-06 15:11:15 +0200

    Note View

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Form\NotesType;
+use App\Entity\Notes;
+use App\Entity\User;
 use App\Repository\NotesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class NotesController extends AbstractController
@@ -21,19 +24,47 @@ class NotesController extends AbstractController
     public function listNotes()
     {   
         $userNotes = $this->notesRepository->findBy(['user' => $this->getUser()]);
+        $note = $this->notesRepository->findAll();
         
         return $this->render('notes/list-notes.html.twig', [
             'userNotes' => $userNotes,
+            'note' => $note
         ]);
     }
   
-    public function note($id)
+    public function note($id,Request $request)
     {   
         $userNotes = $this->notesRepository->findBy(['user' => $this->getUser()]);
         $note = $this->notesRepository->findNote($id);
+
+        $user = $this->getUser();
+
+        $newNote = new Notes();
+        $newNote->setUser($user);
+        $newNote->setInterestProfile($note[0]->getInterestProfile());
+        $newNote->setInterestProject($note[0]->getInterestProject());
+
+        $form = $this->createForm(
+            NotesType::class,
+            $newNote
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {        
+            $newNote->setNotesDate(new \DateTime());
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newNote);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('list-notes');
+        }
+
+
         return $this->render('notes/note.html.twig', [
             'note' => $note,
-            'userNotes'=>$userNotes
+            'userNotes'=>$userNotes,
+            'form' => $form->createView()
         ]);
     }
 }
